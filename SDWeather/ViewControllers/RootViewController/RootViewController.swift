c1d373cf3a8eac801681e367cd760defcb7db44c
Vikas Kumar <kvikas877@gmail.com>
2019-12-25 18:52:44 +0530

    Project setup

---
+++ SDWeather/ViewControllers/RootViewController/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  SDWeather
+//
+//  Created by Vikas Kumar on 25/12/19.
+//  Copyright © 2019 Vikas Kumar. All rights reserved.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
